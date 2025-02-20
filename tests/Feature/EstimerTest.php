861eb9234668563be1c11f0bf06f18ddec80ffc7
yduvignau <yduvignau@snapp.fr>
2025-02-20 22:18:49 +0100

    fix tests

--- tests/Feature/EstimerTest.php
+++ tests/Feature/EstimerTest.php
@@ -1,7 +1,7 @@
 <?php
 
 it('returns a successful (200) response', function () {
-    $data = ["description" => 'Une description d\'annonce.'];
+    $data = ["description" => 'Maison à louer, 5 pièces, 2 chambres.à Bordeaux au Chartrons. À découvrir sans tarder, cette maison de 100 m² environ. 800 euros.'];
     $response = $this->postJson('/api/ia/estimation', $data, ['Accept' => 'application/json']);
 
     $response->assertStatus(200);
